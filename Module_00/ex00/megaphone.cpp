f763927fe72f4dddf877a41a96393cf8dceb05c7
Tiffany Manolis <tiffany.mano@gmail.com>
2022-08-16 16:53:34 +0200

    ex00 done and makefile is cute

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+
+	return 0;
+}
