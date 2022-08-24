0114c315e821f275b61a3d1b63eae984c7cdb932
Tiffany Manolis <tiffany.mano@gmail.com>
2022-08-24 18:01:55 +0200

    protected ifstream if FileName is not a correct file

--- Module_01/ex04/incl/Replace.hpp
+++ Module_01/ex04/incl/Replace.hpp
@@ -11,13 +11,15 @@ private:
 	std::string	const _FileName;
 	std::string	const _Str1; // string you have to replace
 	std::string	const _Str2; // string you replace with
+	std::string	_Content;
 
 public:
 
 	Replace(std::string name, std::string str1, std::string str2);
 	~Replace();
 
-	void	CopyIntoNewFile(void);
+	bool	RetrieveFileContent(void);
+	void	PutContentIntoNewFile(void) const;
 };
 
 /**
@@ -31,15 +33,63 @@ public:
  * useful link : https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
  */
 
-void	Replace::CopyIntoNewFile(void)
+// void	Replace::CopyIntoNewFile(void)
+// {
+// 	std::ifstream		ifs(this->_FileName);
+// 	std::stringstream	buffer;
+// 	std::ofstream		ofs(this->_FileName + ".replace");
+
+// 	//! Need to protect ifs(this->_FileName) before continuing
+// 	buffer << ifs.rdbuf(); // stock file's content into buffer
+// 	ofs << buffer.str(); // transform buffer's content in a string and put it into the new file
+// 	ifs.close();
+// 	ofs.close();
+// }
+
+/**
+ * @brief Read from a file and put its content into string var
+ * 
+ * Used infile stream to manipulate the file :
+ * - check if error occured when we try link the stream with the file and
+ * 	check if is not a directory. 
+ * - std::stringstream buffer allows to stock what has been read from ifs, 
+ * 	using the member function : rdbuf();
+ * - buffer.str() transform content's buffer in a string
+ * 
+ * useful link : 
+ * - https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+ * - https://stackoverflow.com/questions/29310166/check-if-a-fstream-is-either-a-file-or-directory
+ */
+
+bool	Replace::RetrieveFileContent(void)
 {
 	std::ifstream		ifs(this->_FileName);
 	std::stringstream	buffer;
+
+	// Check if infile stream encoutered an error
+	if (ifs.is_open() == false || ifs.fail() == true)
+		return (false);
+	// This will set the fail bit if fileName is a directory (or do nothing if it is already set) 
+	ifs.seekg(0, std::ios::end);
+	if(!ifs.good())
+	{
+		ifs.close();
+		return (false);
+	}
+	// Stock file's content into buffer, then put it into the newfile
+	buffer << ifs.rdbuf();
+	// ! PROBLEME CA NE STOCK PAS dans la string
+	this->_Content = buffer.str();
+	// std::cout << this->_Content << std::endl;
+	ifs.close();
+	return (true);
+}
+
+void	Replace::PutContentIntoNewFile(void) const
+{
 	std::ofstream		ofs(this->_FileName + ".replace");
 
-	buffer << ifs.rdbuf(); // stock file's content into buffer
-	ofs << buffer.str(); // transform buffer's content in a string and put it into the new file
-	ifs.close();
+	ofs << this->_Content;
 	ofs.close();
 }
 
--- Module_01/ex04/srcs/main.cpp
+++ Module_01/ex04/srcs/main.cpp
@@ -10,7 +10,11 @@ int	main(int argc, char *argv[])
 
 	Replace	Replace((std::string)argv[1], (std::string)argv[2], (std::string)argv[3]);
 
-	Replace.CopyIntoNewFile();
-
+	if (Replace.RetrieveFileContent() == false)
+	{
+		std::cout << "Error with the file" << std::endl;
+		return (-1);
+	}
+	Replace.PutContentIntoNewFile();
 	return (0);
 }
