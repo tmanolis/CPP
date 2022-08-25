710e70b3375909482f0d1087f4b44e82b6b8aeac
Tiffany Manolis <tiffany.mano@gmail.com>
2022-08-25 14:35:34 +0200

    everything works just need to add a protection if filename is a directory name

---
+++ Module_01/ex04/srcs/Replace.cpp
@@ -0,0 +1,75 @@
+#include "Replace.hpp"
+
+Replace::Replace(std::string name, std::string str1, std::string str2) : _FileName(name), _Str1(str1), _Str2(str2)
+{
+	std::cout << "FileName : " << this->_FileName << std::endl;
+	std::cout << "String to Replace : " << this->_Str1 << std::endl;
+	std::cout << "String to Replace with : " << this->_Str2 << std::endl;
+}
+
+Replace::~Replace()
+{
+}
+
+/**
+ * @brief Read from a file and put its content into string var
+ * 
+ * Used infile stream to manipulate the file :
+ * - check if error occured when we try link the stream with the file.
+ * - std::stringstream buffer allows to stock what has been read from ifs, 
+ * 	using the member function : rdbuf().
+ * - buffer.str() transform content's buffer in a string.
+ * 
+ * useful link : 
+ * https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+ */
+
+bool	Replace::RetrieveFileContent(void)
+{
+	std::ifstream		ifs(this->_FileName);
+	std::stringstream	buffer;
+
+	// Check if infile stream encoutered an error
+	if (ifs.is_open() == false || ifs.fail() == true)
+		return (false);
+	// Stock file's content into buffer, then put it into a string
+	buffer << ifs.rdbuf();
+	this->_Content = buffer.str();
+	ifs.close();
+	return (true);
+}
+
+/**
+ * @brief Replace substring Str1 by subtring Str2 in the string _Content
+ * 
+ * - Used string::find to searches the string for the first occurrence of the sequence specified :
+ * 		find() returns 'std::string::npos' if there are no occurrences.
+ * - std::string::replace is forbidden, used erase and insert instead.
+ */
+
+void	Replace::ReplaceContent(void)
+{
+	if(this->_Str1.empty())
+        return;
+	
+    size_t start_pos;
+	start_pos = this->_Content.find(this->_Str1, 0);
+
+    while(start_pos != std::string::npos)
+	{
+		// Replace 'Str1' by 'Str2'
+		this->_Content.erase(start_pos, this->_Str1.length());
+		this->_Content.insert(start_pos, this->_Str2);
+        // Look for another occurence of 'Str1' in '_Content'
+        start_pos += this->_Str2.length(); // In case 'Str2' contains 'Str1'
+		start_pos = this->_Content.find(this->_Str1, start_pos);
+    }
+}
+
+void	Replace::PutContentIntoNewFile(void) const
+{
+	std::ofstream		ofs(this->_FileName + ".replace");
+
+	ofs << this->_Content;
+	ofs.close();
+}
--- Module_01/ex04/srcs/main.cpp
+++ Module_01/ex04/srcs/main.cpp
@@ -10,11 +10,24 @@ int	main(int argc, char *argv[])
 
 	Replace	Replace((std::string)argv[1], (std::string)argv[2], (std::string)argv[3]);
 
+	// ! Check de dossier fait bug
+	// https://stackoverflow.com/questions/29310166/check-if-a-fstream-is-either-a-file-or-directory
+	// std::ifstream		ifs(this->_FileName);
+	// This will set the fail bit if fileName is a directory (or do nothing if it is already set) 
+	// ifs.seekg(0, std::ios::end);
+	// if(!ifs.good())
+	// {
+	// 	ifs.close();
+	// 	return (false);
+	// }
+	//
 	if (Replace.RetrieveFileContent() == false)
 	{
-		std::cout << "Error with the file" << std::endl;
+		std::cout << "File error encountered" << std::endl;
 		return (-1);
 	}
+	Replace.ReplaceContent();
 	Replace.PutContentIntoNewFile();
+	
 	return (0);
 }
