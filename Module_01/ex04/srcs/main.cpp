74cb14c90a1820db6811831917d5e591af0b720c
Tiffany Manolis <tiffany.mano@gmail.com>
2022-08-25 14:58:15 +0200

    directory protection but not rly working

--- Module_01/ex04/srcs/main.cpp
+++ Module_01/ex04/srcs/main.cpp
@@ -1,33 +1,60 @@
 #include "Replace.hpp"
 
-int	main(int argc, char *argv[])
+/**
+ * @brief Check if the FileName given in argument is a directory
+ * 
+ * This will set the fail bit if fileName is a directory,
+ * (or do nothing if it is already set.
+ * 
+ * @return true if it is a directory and false if it's not.
+ * 
+ * Useful link : 
+ * https://stackoverflow.com/questions/29310166/check-if-a-fstream-is-either-a-file-or-directory
+ */
+
+bool	is_a_directory(std::string FileName)
+{
+	std::ifstream	ifs(FileName);
+  	
+	ifs.seekg(0, std::ios::end);
+	if (!ifs.good())
+	{
+		ifs.close();
+		return (true);
+	}
+	ifs.close();
+	return (false);
+}
+
+int	check_args(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
 		std::cout << "Usage : ./replace <FileName> <StringToReplace> <StringToReplaceWith>" << std::endl;
 		return (1);
 	}
+	if (is_a_directory(argv[1]) == true)
+	{
+		std::cout << "FileName is a directory" << std::endl;
+		return (1);
+	}
+	return (0);
+}
 
+int	main(int argc, char *argv[])
+{
+	if (check_args(argc, argv))
+		return (1);
+	
 	Replace	Replace((std::string)argv[1], (std::string)argv[2], (std::string)argv[3]);
 
-	// ! Check de dossier fait bug
-	// https://stackoverflow.com/questions/29310166/check-if-a-fstream-is-either-a-file-or-directory
-	// std::ifstream		ifs(this->_FileName);
-	// This will set the fail bit if fileName is a directory (or do nothing if it is already set) 
-	// ifs.seekg(0, std::ios::end);
-	// if(!ifs.good())
-	// {
-	// 	ifs.close();
-	// 	return (false);
-	// }
-	//
 	if (Replace.RetrieveFileContent() == false)
 	{
 		std::cout << "File error encountered" << std::endl;
-		return (-1);
+		return (1);
 	}
 	Replace.ReplaceContent();
 	Replace.PutContentIntoNewFile();
-	
+
 	return (0);
 }
