45cbe429858d73dd31849966e3604ba8de607633
Tiffany Manolis <tiffany.mano@gmail.com>
2022-10-31 20:07:29 +0100

    add easyfind.tpp

--- Module_08/ex00/incl/easyfind.hpp
+++ Module_08/ex00/incl/easyfind.hpp
@@ -14,44 +14,12 @@
 class NoOccurenceException : public std::exception
 {
 	public :
-		virtual char const	*what() const throw()
-		{
-			return ("There is no occurence");
-		};
+		virtual char const	*what() const throw();
 };
 
-/**
- * @brief std::find
- * template <class InputIterator, class T>   InputIterator find (InputIterator first, InputIterator last, const T& val);
- * 
- * Returns an iterator to the first element in the range [first,last) that compares equal to val. 
- * If no such element is found, the function returns last.
- * 
- * @param occurence : is an iterator 
- */
-
 template <typename T>
-typename T::const_iterator	easyfind(T const &haystack, int const needle)
-{
-	typename T::const_iterator	occurence;
+typename T::const_iterator	easyfind(T const &haystack, int const needle);
 
-	occurence = std::find(haystack.begin(), haystack.end(), needle);
-	try
-	{
-		if (occurence != haystack.end())
-		{
-			std::cout << GREEN << "First occurence of " << needle << " was found. " << RESET << std::endl;
-			std::cout << "Position of this occurence is: " << occurence - haystack.begin() << std::endl;
-		}
-		else
-			throw NoOccurenceException();
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << RED << e.what() << "for " << needle << RESET << std::endl;
-	}
-	
-	return (occurence);
-}
+#include "easyfind.tpp"
 
 #endif
